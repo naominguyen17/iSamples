2a500bd3ba1f0917df997923f0b86fbd522ffdaf
naominguyen17 <nguyenng@email.arizona.edu>
2021-12-06 19:43:51 -0700

    Trying to do tree view

--- render.js
+++ render.js
@@ -41,11 +41,60 @@ function makeTable(data, columnNames, tableID) {
 }
 
 function makeTreeList(data, treeID) {
+    // data:
+    // root{
+    //   AllPlants: {
+    //     PlantA: {}
+    //     PlantB: {}
+    //}
+    //}
+    let marginXPerDepth = 10;
     var treeul = d3.select(treeID).append("ul")
                    .classed("treelist", "true");
     var leaves = [];
-    const rootKey = "AllPlants";
     const depth = 4;
-    let nodes
+    var root = {name: "root", data: data, parent: [], children: undefined, leafParent: false};
+    var nodes = [root,
+                 {name: "AllPlants", data: data, parent: [root], children: Object.entries(data["AllPlants"]),
+                leafParent: false}];
+    let visitOrder = [];
+    while(nodes.length > 1) {
+        let v = nodes.pop();
+        let vName=v.name;
+        let vParent=v.parent;
+        let vData=v.data[v.name];
+        let vChildren = v.children;
+        visitOrder.push(v);
+        if(vParent[vParent.length-1].leafParent) {
+            // is leaf
+            console.log(v, "isLeaf");
+            leaves.push(v);
+            continue;
+        }
+        for(let entry of vChildren) {
+            let key=entry[0];
+            let value=entry[1];
+            let parentOfLeaf = typeof(value) == 'string';
+            let nChildren = (parentOfLeaf)? [value]: Object.entries(value);
+            // push children
+            var child = v.leafParent?
+                {name:entry, data: vData, children: [], leafParent:false}
+                :{name: key, data: vData, children: nChildren,
+                leafParent: parentOfLeaf};
+            child.parent = vParent.slice();
+            child.parent.push(v);
+            nodes.push(child);
+        }
 
+    }
+    console.log(visitOrder);
+    console.log("leaves", leaves);
+    var ulSelects = treeul.selectAll("li").data(visitOrder);
+    ulSelects.exit().remove();
+    ulSelects.join("li").text((d)=>d.name)
+        .attr("transform", (d)=>{
+            let txt = "translate("+(marginXPerDepth*d.parent.length)+")";
+            console.log(d.name, txt);
+            return txt;
+        });
 }
